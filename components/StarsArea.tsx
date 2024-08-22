610aaf5038afd334e428011557b4ffd259c8bf41
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-08-22 21:00:28 +0200

    update

--- components/StarsArea.tsx
+++ components/StarsArea.tsx
@@ -67,7 +67,7 @@ export function StarsArea() {
   return (
     <div className="mt-20 py-10 md:py-20 rounded-[40px] bg-neutral-900 flex flex-col items-center justify-center relative w-full px-6 md:px-0">
       <h2 className="relative flex-col  z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-400 via-white to-white flex items-center gap-2 ">
-        Our guarantees to you.
+         Our Services is the best solution for your business.
         <p className="md:text-center   mx-auto  text-xl md:text-2xl text-gray-200">
           We ensure the highest quality of work, with the fastest delivery
           times.
